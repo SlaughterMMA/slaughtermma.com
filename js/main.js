f593c9709009b532c7aa2cc896d6d954f17fc8fd
Justin Gallagher <justingallagher@outlook.com>
2024-12-10 12:39:25 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 var opponentName = "Jordan Frey";
 var eventName = "Bayfront Brawl 16";
-var eventDate = new Date("November 25, 2024 20:00:00");
+var eventDate = new Date("December 25, 2024 20:00:00");
 var eventLocation = "the Bayfront Convention Center";
 var ticketUrl = "https://mpv.tickets.com/?agency=ERIE_EVENTS_MPV&orgid=37168&eventid=4767";
 var ppvUrl = "https://combatsportsnow.com/event/bayfront-brawl-16";
@@ -62,14 +62,4 @@ function ticketOpen(e) {
     e.cancelBubble = true;
     if (e.stopPropagation) e.stopPropagation();
     window.open(ticketUrl, '_blank');
-}
-
-function mouseOut(element) {
-    element.style.color = getComputedStyle(element).getPropertyValue('--main-accent2');
-    element.style.background = black;
-}
-
-function mouseOver(element) {
-    element.style.color = black;
-    element.style.background = getComputedStyle(element).getPropertyValue('--main-accent1');
 }
